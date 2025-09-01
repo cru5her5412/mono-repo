ebbac16e0a04e64e1276f1a89f37027d3b57e3b7
Callum James Lodge <callumjlodge@gmail.com>
2025-09-01 16:46:32 +0100

    experimenting to attempt to fix issues

--- client/client.js
+++ client/client.js
@@ -1,5 +1,5 @@
 async function getMessage() {
-  const response = await fetch("http://mono-repo-h4fs.onrender.com/");
+  const response = await fetch("https://mono-repo-h4fs.onrender.com/");
   const message = await response.json();
   const placeForMessage = document.getElementById("message");
   placeForMessage.textContent = message;
