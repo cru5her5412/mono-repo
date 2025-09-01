72133952aec25a6546b507177c78a00ae9ab04ae
Callum James Lodge <callumjlodge@gmail.com>
2025-09-01 16:12:15 +0100

    added code behind client and server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express, { response } from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.get("/", function (request, response) {
+  response.json("hi");
+});
+app.listen(8080, function () {
+  console.log("listening on port 8080");
+});
