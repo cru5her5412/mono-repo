bd1c8c948945550cb73aa7fdf3bdda4678e06359
Callum James Lodge <callumjlodge@gmail.com>
2025-09-01 16:42:51 +0100

    added correct url for server

--- client/client.js
+++ client/client.js
@@ -1,5 +1,5 @@
 async function getMessage() {
-  response = await fetch("https://mono-repo-h4fs.onrender.com/");
+  response = await fetch("http://mono-repo-h4fs.onrender.com/");
   message = await response.json();
   placeForMessage = document.getElementById("message");
   placeForMessage.textContent = message;
